ea2df245f97ebe8091ce4c092df00860fce04174
kenya6111 <kenyanke6111@gmail.com>
2025-05-25 15:40:44 +0900

    2.7 デコーダ・エンコーダを使ってリファクタ

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -23,42 +22,17 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) { //curl http://loca
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	// 1. バイトスライス reqBodybuffer を何らかの形で用意
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-		return
-	}
-	reqBodybuffer := make([]byte, length)
-	// 2. Read メソッドでリクエストボディを読み出し
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-	// 3. ボディを Close する
-	defer req.Body.Close()
 	var reqArticle models.Article
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
 	}
+
 	article := reqArticle
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-		return
-	}
-	w.Write(jsonData)
+
+	json.NewEncoder(w).Encode(article)
 }
 
 func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	var reqBodyBuffer []byte
-	if _, err := req.Body.Read(reqBodyBuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "failed to read request body\n", http.StatusInternalServerError)
-		return
-	}
-
-	defer req.Body.Close()
 
 	queryMap := req.URL.Query()
 
@@ -74,49 +48,44 @@ func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		page = 1
 	}
 
-	articleList := []models.Article{models.Article1, models.Article2}
-	jsonData, err := json.Marshal(articleList)
-	if err != nil {
-		_, errMsg := fmt.Printf("failed to encode json (page %d)\n", page)
-		http.Error(w, errMsg.Error(), http.StatusInternalServerError)
-	}
+	fmt.Println(page)
 
-	w.Write(jsonData)
+	articleList := []models.Article{models.Article1, models.Article2}
+	json.NewEncoder(w).Encode(articleList)
 }
 
-func GetArticleHandler(w http.ResponseWriter, req *http.Request) {
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 		return
 	}
+	fmt.Println(articleID)
 
 	article := models.Article1
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		_, errMsg := fmt.Printf("failed to encode json (page %d)\n", articleID)
-		http.Error(w, errMsg.Error(), http.StatusInternalServerError)
-	}
-
-	w.Write(jsonData)
+	json.NewEncoder(w).Encode(article)
 }
 
 func PostingNiceHandler(w http.ResponseWriter, req *http.Request) {
-	article := models.Article1
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		http.Error(w, "failed to encode json\n", http.StatusInternalServerError)
-		return
+	// article := models.Article1
+
+	// json.NewEncoder(w).Encode(article)
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
-	w.Write(jsonData)
+
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
+
 }
 
 func CommentHandler(w http.ResponseWriter, req *http.Request) {
-	comment := models.Comment1
-	jsonData, err := json.Marshal(comment)
-	if err != nil {
-		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-		return
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
-	w.Write(jsonData)
+
+	comment := reqComment
+	json.NewEncoder(w).Encode(comment)
 }
--- main.go
+++ main.go
@@ -2,89 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/yourname/reponame/handlers"
 )
 
-type Test struct {
-	name string
-	age  int
-}
-
-func (t *Test) printName() {
-	fmt.Println("name: ", t.name)
-}
-
-func (t Test) addAge1() {
-	t.age += 10
-}
-func (t *Test) addAge2() {
-	t.age += 10
-}
-
-func (t Test) getNameAndAge() string {
-	return "名前は" + t.name + "です。年齢は" + strconv.Itoa(t.age) + "歳です。"
-}
-
-type Test2 struct {
-	name string
-	age  int
-}
-
-func (t Test2) getNameAndAge() string {
-	return "名前は" + t.name + "です。年齢は" + strconv.Itoa(t.age) + "歳です。"
-}
-
-func displayNameAndAge(t Test) {
-	fmt.Println(t.getNameAndAge())
-}
-func displayNameAndAge2(t Test2) {
-	fmt.Println(t.getNameAndAge())
-}
-
-type TestInterface interface {
-	getNameAndAge() string
-}
-
-func superDisplayNameAndAge(t TestInterface) {
-	fmt.Println(t.getNameAndAge())
-}
-
-// Carのinterface
-// Carの機能が関数として入っている
-type Car interface {
-	run(int) string
-	stop()
-}
-
-// MyCarの構造体を定義
-// 構造体は頭大文字
-type MyCar struct {
-	name  string
-	speed int
-}
-
-func (u *MyCar) run(speed int) string {
-	u.speed = speed
-	return strconv.Itoa(speed) + "kmで走ります"
-}
-
-func (u *MyCar) stop() {
-	fmt.Println("停止します")
-	u.speed = 0
-}
-
 type Comment struct {
 	CommentID int       `json:"comment_id"`
 	ArticleID int       `json:"article_id"`
 	Message   string    `json:"message"`
-	createdAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Article struct {
@@ -99,42 +29,17 @@ type Article struct {
 
 func main() {
 
-	student1 := Test{
-		name: "kenya",
-		age:  20,
-	}
-	fmt.Println(student1)
-	fmt.Println(student1.name)
-	fmt.Println(student1.age)
-	student1.printName()
-	student1.addAge1()
-	fmt.Println(student1.age)
-	student1.addAge2()
-	fmt.Println(student1.age)
-	fmt.Println(student1.getNameAndAge())
-	displayNameAndAge(student1)
-
-	student2 := Test2{
-		name: "kenya",
-		age:  20,
-	}
-
-	fmt.Println(student2.getNameAndAge())
-	displayNameAndAge2(student2)
-
-	superDisplayNameAndAge(student1)
-	superDisplayNameAndAge(student2)
 	comment1 := Comment{
 		CommentID: 1,
 		ArticleID: 1,
 		Message:   "test comment1",
-		createdAt: time.Now(),
+		CreatedAt: time.Now(),
 	}
 	comment2 := Comment{
 		CommentID: 2,
 		ArticleID: 1,
 		Message:   "test comment2",
-		createdAt: time.Now(),
+		CreatedAt: time.Now(),
 	}
 	article := Article{
 		ID:          1,
@@ -145,16 +50,7 @@ func main() {
 		CommentList: []Comment{comment1, comment2},
 		CreatedAt:   time.Now(),
 	}
-
 	fmt.Printf("%+v\n", article)
-
-	testHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "hello world \n")
-
-	}
-	http.HandleFunc("/test", testHandler)
-
-	fmt.Printf("%T\n", handlers.HelloHandler)
 
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
@@ -162,10 +58,9 @@ func main() {
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
 	r.HandleFunc("/article/list", handlers.GetArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.GetArticleHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article/nice", handlers.PostingNiceHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", handlers.CommentHandler).Methods(http.MethodPost)
-	r.HandleFunc("/test", testHandler).Methods(http.MethodPost)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 
